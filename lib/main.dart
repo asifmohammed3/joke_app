6df814c8895f91b0d35eecddc1043cf0f8b974dd
Mohammed Asif <82872316+asifmohammed3@users.noreply.github.com>
2023-02-22 21:15:29 +0530

    final

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:joke_app/presentation/Home/home.dart';
+import 'package:joke_app/repository/joke_repostory.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.grey[900],
+        backgroundColor: Colors.grey[900],
+        scaffoldBackgroundColor: Colors.grey[900],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.grey[900],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+home: RepositoryProvider(
+      create: (context) => JokeRepository(),
+      child: const Home(),
+    ),
+    );
+  }
+}
